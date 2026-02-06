5ec541ede65e73aec2f9848f1556148af86787d3
Harshaditya Sharma <harshaditya2209@gmail.com>
2026-02-06 19:14:12 +0530

    small bug fix

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CUR_DIR=$(pwd)
-cd ~/Documents/gator/sql/schema/ || { echo "Schema directory not found!"; exit 1; }
-goose postgres "postgres://postgres:postgres@localhost:5432/gator" down
-goose postgres "postgres://postgres:postgres@localhost:5432/gator" up
-cd "$CUR_DIR"
-echo "Database migrations reset successfully and returned to $CUR_DIR"
-
